f6a7373f3633ccfb04b4e927dfc3afb08f6f8c14
Bernardo Robaina <bernardo.robaina@serel.com.br>
2025-05-12 16:45:01 -0300

    Updates logic to only check profit if the value is not the min

--- main.c
+++ main.c
@@ -2,23 +2,24 @@
 
 int maxProfit(int* prices, int pricesSize){
     int min = 0;
-    int minIndex = 0;
-    int max = 0;
-    int maxIndex = 0;
+    int profit = 0;
+
     for(int i = 0; i < pricesSize; i++){
         if(prices[i] < min || i == 0){
             min = prices[i];
-            minIndex = i;
+        }else{
+            if(prices[i] - min > profit){
+                profit = prices[i] - min;
+            }
         }
-        if(prices[i] > max || i == 0){
-            max = prices[i];
-            maxIndex = i;
-        }
+
     }
+
+    return profit;
 }
 
 void main(){
     int prices[] = {7,1,5,3,6,4};
 
-    maxProfit(prices, sizeof(prices)/sizeof(int));
+    printf("%d", maxProfit(prices, sizeof(prices)/sizeof(int)));
 }
